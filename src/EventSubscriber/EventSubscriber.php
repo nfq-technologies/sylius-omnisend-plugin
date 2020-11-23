2a67ce2e8deace490cdc89266797559e622d8cd3
Aurimas Valancius <aurimas.valancius@nfq.lt>
2020-11-23 14:21:59 +0200

    EPLGN-9. Added event subscriber

---
+++ src/EventSubscriber/EventSubscriber.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * @copyright C UAB NFQ Technologies
+ *
+ * This Software is the property of NFQ Technologies
+ * and is protected by copyright law – it is NOT Freeware.
+ *
+ * Any unauthorized use of this software without a valid license key
+ * is a violation of the license agreement and will be prosecuted by
+ * civil and criminal law.
+ *
+ * Contact UAB NFQ Technologies:
+ * E-mail: info@nfq.lt
+ * http://www.nfq.lt
+ */
+
+declare(strict_types=1);
+
+namespace NFQ\SyliusOmnisendPlugin\EventSubscriber;
+
+use NFQ\SyliusOmnisendPlugin\Message\Command\UpdateEvent;
+use NFQ\SyliusOmnisendPlugin\Model\Event;
+use Sylius\Bundle\ResourceBundle\Event\ResourceControllerEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+class EventSubscriber implements EventSubscriberInterface
+{
+    /** @var MessageBusInterface */
+    private $messageBus;
+
+    public function __construct(
+        MessageBusInterface $messageBus
+    ) {
+        $this->messageBus = $messageBus;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'nfq_sylius_omnisend_plugin.event.post_update' => 'onEventChange',
+            'nfq_sylius_omnisend_plugin.event.post_create' => 'onEventChange',
+        ];
+    }
+
+    public function onEventChange(ResourceControllerEvent $event): void
+    {
+        /** @var Event $trackingEvent */
+        $trackingEvent = $event->getSubject();
+
+        $this->messageBus->dispatch(
+            new Envelope(
+                new UpdateEvent(
+                    $trackingEvent->getSystemName(),
+                    $trackingEvent->getChannel()->getCode()
+                )
+            )
+        );
+    }
+}
