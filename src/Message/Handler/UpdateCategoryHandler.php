957bfddca6d39679bb585cd8604e5aa5a987e720
Aurimas Valancius <aurimas.valancius@nfq.lt>
2020-11-24 21:47:55 +0200

    EPLGN-5. Fixes after merge request

--- src/Message/Handler/UpdateCategoryHandler.php
+++ src/Message/Handler/UpdateCategoryHandler.php
@@ -53,25 +53,23 @@ class UpdateCategoryHandler implements MessageHandlerInterface
         /** @var TaxonInterface|null $taxon */
         $taxon = $this->taxonRepository->findOneBy(['code' => $message->getTaxonCode()]);
 
-        if (null !== $taxon) {
-            if ($taxon->isPushedToOmnisend()) {
-                $response = $this->omnisendClient->putCategory($this->categoryFactory->create($taxon), $message->getChannelCode());
-
-                if (null !== $response) {
-                    $taxon->setPushedToOmnisend(new DateTime());
-                    $this->taxonRepository->add($taxon);
-                }
-            } else {
-                $response = $this->omnisendClient->postCategory(
-                    $this->categoryFactory->create($taxon),
-                    $message->getChannelCode()
-                );
-
-                if (null !== $response) {
-                    $taxon->setPushedToOmnisend(new DateTime());
-                    $this->taxonRepository->add($taxon);
-                }
-            }
+        if (null === $taxon) {
+            return;
         }
+
+        if ($taxon->isPushedToOmnisend()) {
+            $response = $this->omnisendClient->putCategory($this->categoryFactory->create($taxon), $message->getChannelCode());
+        } else {
+            $response = $this->omnisendClient->postCategory(
+                $this->categoryFactory->create($taxon),
+                $message->getChannelCode()
+            );
+        }
+
+        if (null !== $response) {
+            $taxon->setPushedToOmnisend(new DateTime());
+            $this->taxonRepository->add($taxon);
+        }
+        
     }
 }
