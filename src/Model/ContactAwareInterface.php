0d8bb429af798031b953d4841ed27a3970e881da
Aurimas Valancius <aurimas.valancius@nfq.lt>
2020-11-26 00:45:08 +0200

    EPLGN-0. Updated copyright

--- src/Model/ContactAwareInterface.php
+++ src/Model/ContactAwareInterface.php
@@ -1,18 +1,12 @@
 <?php
 
 /*
- * @copyright C UAB NFQ Technologies
+ * This file is part of the NFQ package.
  *
- * This Software is the property of NFQ Technologies
- * and is protected by copyright law – it is NOT Freeware.
+ * (c) NFQ Technologies UAB <info@nfq.com>
  *
- * Any unauthorized use of this software without a valid license key
- * is a violation of the license agreement and will be prosecuted by
- * civil and criminal law.
- *
- * Contact UAB NFQ Technologies:
- * E-mail: info@nfq.lt
- * http://www.nfq.lt
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace NFQ\SyliusOmnisendPlugin\Model;
