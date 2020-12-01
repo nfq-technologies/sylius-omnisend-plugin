1b1aa1ad811c27aa215e786ac0e045a34ac7b2be
Aurimas Valancius <aurimas.valancius@nfq.lt>
2020-12-01 10:43:38 +0200

    EPLGN-0. Updated copyright

--- src/Client/Request/Model/Contact.php
+++ src/Client/Request/Model/Contact.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the NFQ package.
  *
- * (c) NFQ Technologies UAB <info@nfq.com>
+ * (c) Nfq Technologies UAB <info@nfq.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- src/Model/ContactAwareInterface.php
+++ src/Model/ContactAwareInterface.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of the NFQ package.
  *
- * (c) NFQ Technologies UAB <info@nfq.com>
+ * (c) Nfq Technologies UAB <info@nfq.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
