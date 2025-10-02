49db0b9f74f01945c5f3972506baf8fb6f7c53b8
Justinas Urbanavičius <justinas.urbanavicius@nfq.com>
2025-10-02 15:27:39 +0300

    Improved retry logic for custom events
    - don't discard validation failed events, but don't retry on failure
    - Don't discard not found events, retry instead

--- src/Message/Handler/PushCustomEventHandler.php
+++ src/Message/Handler/PushCustomEventHandler.php
@@ -15,59 +15,49 @@ namespace NFQ\SyliusOmnisendPlugin\Message\Handler;
 
 use NFQ\SyliusOmnisendPlugin\Builder\Request\EventBuilderDirectorInterface;
 use NFQ\SyliusOmnisendPlugin\Client\OmnisendClientInterface;
+use NFQ\SyliusOmnisendPlugin\Exception\SystemEventNotFoundException;
 use NFQ\SyliusOmnisendPlugin\Message\Command\PushCustomEvent;
 use NFQ\SyliusOmnisendPlugin\Model\Event as BaseEvent;
 use NFQ\SyliusOmnisendPlugin\Validator\Constraints\CustomEventFields;
 use Psr\Log\LoggerAwareTrait;
 use Sylius\Resource\Doctrine\Persistence\RepositoryInterface;
+use Symfony\Component\Messenger\Exception\UnrecoverableMessageHandlingException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+use function sprintf;
 
 class PushCustomEventHandler
 {
     use LoggerAwareTrait;
 
-    /** @var RepositoryInterface */
-    private $eventRepository;
-
-    /** @var EventBuilderDirectorInterface */
-    private $eventBuilderDirector;
-
-    /** @var OmnisendClientInterface */
-    private $omnisendClient;
-
-    /** @var ValidatorInterface */
-    private $validator;
-
     public function __construct(
-        RepositoryInterface $eventRepository,
-        EventBuilderDirectorInterface $eventBuilderDirector,
-        OmnisendClientInterface $omnisendClient,
-        ValidatorInterface $validator
+        private readonly RepositoryInterface $eventRepository,
+        private readonly EventBuilderDirectorInterface $eventBuilderDirector,
+        private readonly OmnisendClientInterface $omnisendClient,
+        private readonly ValidatorInterface $validator
     ) {
-        $this->eventRepository = $eventRepository;
-        $this->eventBuilderDirector = $eventBuilderDirector;
-        $this->omnisendClient = $omnisendClient;
-        $this->validator = $validator;
     }
 
     public function __invoke(PushCustomEvent $message): void
     {
         $violations = $this->validator->validate($message, new CustomEventFields());
         if ($violations->count() > 0) {
-            if (null !== $this->logger) {
-                $this->logger->error(
-                    'Omnisend custom event {systemName} cannot be pushed.',
-                    ['errors' => $violations, 'systemName' => $message->getSystemName()],
-                );
-            }
+            $this->logger?->error(
+                'Omnisend custom event {systemName} cannot be pushed.',
+                ['errors' => $violations, 'systemName' => $message->getSystemName()],
+            );
 
-            return;
+            throw new UnrecoverableMessageHandlingException(
+                sprintf('Custom event %s validation failed', $message->getSystemName()),
+            );
         }
         /** @var BaseEvent|null $event */
         $event = $this->eventRepository->findOneBy(['systemName' => $message->getSystemName()]);
 
-        if (null === $event) {
-            return;
+        if ($event === null) {
+            throw new SystemEventNotFoundException(
+                sprintf('Event with systemName %s not found', $message->getSystemName()),
+            );
         }
 
         $this->omnisendClient->postEvent(
